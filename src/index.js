caa85ffec45fdf03d2114fbf1e42786d308130fc
Abel Seifu <abel.workmail@gmail.com>
2022-10-13 20:43:47 +0300

    Full initial setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import _ from 'lodash';
+import './style.css';
 
  function component() {
    const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
+   
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
    return element;
