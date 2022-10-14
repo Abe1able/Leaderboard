988684accd5a14b71f16fa754d4e33fdf3fc458e
Abel Seifu <abel.workmail@gmail.com>
2022-10-14 13:55:58 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import './style.css';
 import fetchData from './modules/fetchDataFromAPI';
 import addScore from './modules/addScore';
     
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/puvcouP5rfhcu4N1P4RK/scores';
-
 const submitForm = document.querySelector('form');
 const playerScore = document.querySelector('#score');
 const playerName = document.querySelector('#name');
